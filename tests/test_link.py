087167dcfd36e6098fb946bbf0aad4d6493acc33
satamame <satax@nifty.com>
2024-10-14 13:12:21 +0900

    test: Add tests for hyperlinks to in-file targets.

--- tests/test_link.py
+++ tests/test_link.py
@@ -1,3 +1,4 @@
+import pytest
 from lxml import html
 
 
@@ -38,6 +39,17 @@ class TestLink:
         tree = html.parse(sphinx_app.outdir / "svg_inline/index.html")
         href = 'https://google.com'
         text = 'To Absolute URL'
+        xpath = f'//svg//a[@href="{href}" and text()="{text}"]'
+        elements = tree.xpath(xpath)
+        assert len(elements) == 1
+
+    @pytest.mark.sphinx_config(inline_svg_classes=['custom-svg'])
+    def test_link_to_infile_target(self, sphinx_app):
+        '''Test if hyperlink to in-file target is generated
+        '''
+        tree = html.parse(sphinx_app.outdir / "svg_inline/index.html")
+        href = '#target-2'
+        text = 'To in-file target'
         xpath = f'//svg//a[@href="{href}" and text()="{text}"]'
         elements = tree.xpath(xpath)
         assert len(elements) == 1
