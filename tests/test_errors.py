5a44bae47d4d1743de4c2b377864a200b9e436f8
satamame <satax@nifty.com>
2024-10-15 23:48:37 +0900

    test: Add tests for invalid hyperlink error.

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,13 +1,14 @@
 import shutil
 from pathlib import Path
 
+import pytest
 from lxml import etree
 from pytest_mock.plugin import MockerFixture
 from sphinx.testing.util import SphinxTestApp
 
 
 def test_no_svg_element(mocker: MockerFixture):
-    '''Test if exception raises when no svg element is in SVG file
+    '''Test warning log for no svg element in SVG file
     '''
     # Mock `html.parse` function to return a single empty root element.
     mock_html_parse = mocker.patch('sphinx_inline_svg.inline_svg.html.parse')
@@ -23,4 +24,20 @@ def test_no_svg_element(mocker: MockerFixture):
     warnings = app._warning.getvalue()
     app.cleanup()
 
-    assert "No svg element." in warnings
+    assert 'No svg element.' in warnings
+
+
+@pytest.mark.sphinx_config(inline_svg_classes=['undefined-target-svg'])
+def test_link_to_undefined_target(sphinx_app):
+    '''Test warning log for hyperlink to undefined target
+    '''
+    warnings = sphinx_app._warning.getvalue()
+    assert 'Target not found: undefined-target' in warnings
+
+
+@pytest.mark.sphinx_config(inline_svg_classes=['no-file-to-link-svg'])
+def test_link_to_unexisting_file(sphinx_app):
+    '''Test warning log for hyperlink to unexisting file
+    '''
+    warnings = sphinx_app._warning.getvalue()
+    assert 'Document not found: unexisting.txt' in warnings
